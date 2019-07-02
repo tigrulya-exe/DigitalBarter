1a65ac364cbd1ab338e79918cb605fa2461e9c84
Tigran Manasyan <manasyan_tigran@mail.ru>
2019-07-02 15:11:41 +0700

    +Service class was added

---
+++ src/main/java/cft/shift/manasyan/barter/repositories/OfferRepository.java
@@ -0,0 +1,10 @@
+package cft.shift.manasyan.barter.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository {
+    void addOffer();
+
+
+}
---
+++ src/main/java/cft/shift/manasyan/barter/services/DigitalBarterService.java
@@ -0,0 +1,8 @@
+package cft.shift.manasyan.barter.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DigitalBarterService {
+
+}
